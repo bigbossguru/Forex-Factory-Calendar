ba7fccfb008c2ec3a7773f03a1592b82cafc4aec
Eldar MSI <kursus1010@gmail.com>
2024-10-22 20:39:30 +0200

    Clean output directory, changed task every 30 minut

--- scripts/set_cronjob.sh
+++ scripts/set_cronjob.sh
@@ -5,8 +5,8 @@ set -e
 
 cd ../
 echo "Root repository path: $(pwd)"
-(crontab -l 2>/dev/null; echo "*/5 * * * * $(pwd)/.venv/bin/python3 $(pwd)/scraper.py") | crontab -
+(crontab -l 2>/dev/null; echo "*/30 * * * * $(pwd)/.venv/bin/python3 $(pwd)/scraper.py") | crontab -
 
 # Confirm set crontab task
-echo "✅ Set schedule task every day at 17:00 CEST"
+echo "✅ Set scheduled task collect data every 30 minut"
 crontab -l
